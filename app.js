7736b9649f7354617eb9c0034bc1f06f96d6b911
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-11-04 11:51:28 -0800

    Pushed

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const PORT = 3008;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.get("/friends", (req, res) => {
+    const friends = ["Mickey", "Goofy", "Minnie", "Daisy"];
+    res.render('friends', {friends});
+})
+app.get("/movies", (req, res) => {
+    const movies = ["Mickey Two A Fate", "Goofy Goobers in Space!", "Minnie Mouse and the Mysterious Purse", "Daisy Doo and the Crimes of Who?"];
+    res.render('movies', {movies});
+})
+
 app.get('/', (req, res) => {
     const person = { name: 'Donald Duck', occupation: 'plumber', favColor: 'beige' };
     res.render('home', { person });
